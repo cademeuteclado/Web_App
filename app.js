b3d476e9e51e32241bbd7d91dc3d2f11bdd39f06
cademeuteclado <weslley2922@gmail.com>
2023-11-16 11:18:28 -0300

    awd

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-// Função para abrir uma janela específica ao clicar no botão 1
-function openWindow1() {
-  window.open('pagina1.html', '_blank', 'width=500,height=500');
-}
+document.addEventListener('DOMContentLoaded', function() {
+  function activateAnimation() {
+    this.classList.toggle('active');
+    // Redirecionar para a nova página na mesma janela
+    window.location.href = 'pagina_nova.html';
+  }
 
-// Função para abrir uma janela específica ao clicar no botão 2
-function openWindow2() {
-  window.open('pagina2.html', '_blank', 'width=500,height=500');
-}
+  // Selecionar o botão
+  var button = document.getElementById('button1');
 
-// Função para abrir uma janela específica ao clicar no botão 3
-function openWindow3() {
-  window.open('pagina3.html', '_blank', 'width=500,height=500');
-}
-
-// Seleciona os botões com os IDs específicos e adiciona eventos de clique a eles
-document.getElementById('button1').addEventListener('click', openWindow1);
-document.getElementById('button2').addEventListener('click', openWindow2);
-document.getElementById('button3').addEventListener('click', openWindow3);
+  // Adicionar evento de clique ao botão, se encontrado
+  if (button) {
+    button.addEventListener('click', activateAnimation);
+  }
+});
